a7b07edf8989624942d1a5c7c3044487d2633a3e
Su-Hyun Park <lovehyun@hotmail.com>
2023-06-26 21:32:25 +0900

    [BUG] 차트 스코어 그래프 오류 수정

--- 10.Projects/MultiPlayerGame2/client/modules/stats/clientstats.js
+++ 10.Projects/MultiPlayerGame2/client/modules/stats/clientstats.js
@@ -1,104 +1,124 @@
 const clientStatsContainer = document.getElementById('clientStatsContainer');
 
+// 차트 생성 함수
+function createConnectionBarChart(data) {
+    const ctx = document.getElementById('clientStatsChart').getContext('2d');
+
+    const labels = [];
+
+    // 챠트 정보 생성 - ClientIP 추출
+    data.forEach(client => {
+        labels.push(client.IP);
+    });
+
+    const dataset_data = data.map(client => client.totalConnections);
+
+    const chart_data = {
+        labels: labels,
+        datasets: [
+            {
+                label: 'IP Address',
+                data: dataset_data,
+                backgroundColor: 'rgba(75, 192, 192, 0.6)',
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1
+            }
+        ]
+    };
+
+    const chart_options = {
+        indexAxis: 'y',
+        responsive: true,
+        scales: {
+            x: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Total Connections'
+                }
+            },
+            y: {
+                title: {
+                    display: true,
+                    text: 'IP Address'
+                }
+            }
+        },
+        barThickness: 20
+    }
+
+    new Chart(ctx, {
+        type: 'bar',
+        data: chart_data,
+        options: chart_options,
+    });
+}
+
+// 차트 생성 함수
+function createScoreLineChart(data) {
+    const ctx = document.getElementById('clientScoreChart').getContext('2d');
+
+    const labels = [];
+    const scores_map = [];
+
+    // 챠트 정보 생성
+    data.forEach((client) => {
+        const scores = client.sessions.map(session => session.score !== null ? session.score : 0);
+        labels.push(client.IP);
+        scores_map.push(scores);
+    });
+
+    // 차트 스코어 데이터 생성
+    const datasets = scores_map.map((scores, index) => ({
+        label: labels[index],
+        data: scores,
+        backgroundColor: `rgba(75, 192, 192, ${0.2 + (index * 0.1)})`,
+        borderColor: `rgba(75, 192, 192, 1)`,
+        borderWidth: 1
+    }));
+
+    const chart_data = {
+        labels: Array.from({ length: scores_map[0].length }, (_, i) => i + 1),
+        datasets: datasets
+    };
+
+    const chart_options = {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true,
+                title: {
+                    display: true,
+                    text: 'Score'
+                }
+            },
+            x: {
+                type: 'linear',
+                title: {
+                    display: true,
+                    text: 'Game Round'
+                },
+                ticks: {
+                    stepSize: 1.0
+                }
+            }
+        }
+    };
+
+    new Chart(ctx, {
+        type: 'line',
+        data: chart_data,
+        options: chart_options,
+    });
+}
+
 fetch('/api/clients/stats')
     .then(response => response.json())
     .then(data => {
-        const stats = data;
-
-        // 데이터 추출
-        const labels = stats.map(client => client.IP);
-        const connections = stats.map(client => client.totalConnections);
-        const sessionCounts = stats.map(client => client.sessions.length);
-        const scores = [];
-
-        for (let i = 0; i < sessionCounts.length; i++) {
-            for (let j = 0; j < sessionCounts[i]; j++) {
-                const score = stats[i].sessions[j].score;
-                scores.push(score !== null ? score : 0);
-            }
-        }
-
-        const scores_map = scores.map((score, index) => ({ x: index + 1, y: score }));
-
-        console.log("connections: ", connections);
-        console.log("scores_map: ", sessionCounts, scores, scores_map);
-
-        // 차트 생성
-        let ctx = document.getElementById('clientStatsChart').getContext('2d');
-        new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'IP Address', // x 축 제목 변경
-                        data: connections,
-                        backgroundColor: 'rgba(75, 192, 192, 0.6)', // 막대 색상
-                        borderColor: 'rgba(75, 192, 192, 1)', // 막대 테두리 색상
-                        borderWidth: 1 // 막대 테두리 두께
-                    }
-                ]
-            },
-            options: {
-                indexAxis: 'y',
-                responsive: true, // 반응형 크기 조정
-                scales: {
-                    x: {
-                        beginAtZero: true, // x 축 값 0부터 시작
-                        title: {
-                            display: true,
-                            text: 'Total Connections' // x 축 제목 변경
-                        }
-                    },
-                    y: {
-                        title: {
-                            display: true,
-                            text: 'IP Address' // y 축 제목 변경
-                        }
-                    }
-                },
-                barThickness: 20 // 바의 폭 조절
-            }
-        });
-
-        // 차트 생성
-        ctx = document.getElementById('clientScoreChart').getContext('2d');
-        new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: labels,
-                        data: scores_map,
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        borderWidth: 1
-                    }
-                ]
-            },
-            options: {
-                responsive: true,
-                scales: {
-                    y: {
-                        beginAtZero: true,
-                        title: {
-                            display: true,
-                            text: 'Score'
-                        }
-                    },
-                    x: {
-                        type: 'linear', // 선형 스케일링 활성화
-                        title: {
-                            display: true,
-                            text: 'Game Round'
-                        }
-                    }
-                }
-            }
-        });
+        // 차트 생성 함수 호출
+        createConnectionBarChart(data);
+        createScoreLineChart(data);
     })
-
     .catch(error => {
         console.error('오류:', error);
         clientStatsContainer.textContent = '클라이언트 통계를 가져오는 동안 오류가 발생했습니다.';
