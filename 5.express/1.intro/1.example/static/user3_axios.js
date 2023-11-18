10f23619597e94decd12ccf29ff21b50dfb41355
Su-Hyun Park <lovehyun@hotmail.com>
2023-11-18 15:33:08 +0900

    [DEV] fetch/axios 예제 추가

---
+++ 5.express/1.intro/1.example/static/user3_axios.js
@@ -0,0 +1,54 @@
+// 1. npm install axios webpack webpack-cli --save
+// 2. webpack.config.js
+// module.exports = {
+//     entry: './index.js',
+//     output: {
+//       filename: 'bundle.js',
+//     },
+// };
+// 3. npx webpack
+// 4. <script src="bundle.js"></script>
+//
+// 또는, 외부 상위 html 파일에서 로딩...
+// <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
+// 사용자 정보를 받아오는 함수
+export async function getUsers() {
+    try {
+        const response = await axios.get('/user');
+        return response.data;
+    } catch (error) {
+        console.error('사용자 정보 불러오기 실패:', error);
+        throw error; // 에러를 다시 호출자에게 전파
+    }
+}
+
+// 사용자 추가 함수
+export async function addUser(name) {
+    try {
+        await axios.post('/user', { name });
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : '등록 실패';
+        throw new Error(`등록 실패: ${errorMessage}`);
+    }
+}
+
+// 사용자 정보를 업데이트하는 함수
+export async function updateUser(userId, data) {
+    try {
+        await axios.put(`/user/${userId}`, data);
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : '수정 실패';
+        throw new Error(`수정 실패: ${errorMessage}`);
+    }
+}
+
+// 사용자 정보를 삭제하는 함수
+export async function deleteUserById(userId) {
+    try {
+        await axios.delete(`/user/${userId}`);
+    } catch (error) {
+        const errorMessage = error.response ? error.response.data : '삭제 실패';
+        throw new Error(`삭제 실패: ${errorMessage}`);
+    }
+}
--- 5.express/1.intro/app4_mid.js
+++ 5.express/1.intro/app4_mid.js
@@ -13,7 +13,7 @@ var myLogger = function (req, res, next) {
     // console.log('LOGGED: ', req.requestTime);
     const date = new Date(req.requestTime);
     const formattedTime = date.toLocaleString();
-    console.log("LOGGED: ", formattedTime);
+    console.log('LOGGED: ', formattedTime);
     next();
 };
 
@@ -28,6 +28,38 @@ app.get('/', (req, res) => {
     res.send(responseText);
 });
 
+// ----------------------------------------------
+// about 요청 핸들러에 미들웨어들 추가
+app.get('/about', 함수1, 함수2, 함수3, 함수4, (req, res) => {
+    // 라우트 핸들러에서 수행할 작업
+    res.send('GET 요청이 왔어요!');
+});
+
+// 미들웨어 1
+function 함수1(req, res, next) {
+    console.log('미들웨어 1');
+    next();
+}
+
+// 미들웨어 2
+function 함수2(req, res, next) {
+    console.log('미들웨어 2');
+    next();
+}
+
+// 미들웨어 3
+function 함수3(req, res, next) {
+    console.log('미들웨어 3');
+    next();
+}
+
+// 미들웨어 4
+function 함수4(req, res, next) {
+    console.log('미들웨어 4');
+    next();
+}
+
+// ----------------------------------------------
 // 서버 시작
 app.listen(port, () => {
     console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
