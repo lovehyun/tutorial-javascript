f41bf45dffef83dabc229edfe6cff7694aa16081
Su-Hyun Park <lovehyun@hotmail.com>
2023-11-18 16:31:50 +0900

    [DEV] board 에서 better-sqlite코드 추가

--- 6.express_projects/2.board/app2.js
+++ 6.express_projects/2.board/app2.js
@@ -2,8 +2,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
 const bodyParser = require('body-parser');
-const Database = require('./database');
-// const Database = require('./database2');
+const Database = require('./database2');
 
 const app = express();
 const port = 3000;
@@ -35,12 +34,9 @@ app.post('/create', async (req, res) => {
         const message = req.body.message;
         // SQL 쿼리에 prepared statement 사용
         const sql = `INSERT INTO board(title, message) VALUES(?, ?)`;
-        await db.beginTransaction(); // 트랜잭션 시작
-        await db.execute(sql, [title, message]);
-        await db.commit(); // 트랜잭션 커밋
+        const result = db.execute(sql, [title, message]);
         res.json({ 'result': 'success' });
     } catch (error) {
-        await db.rollback(); // 오류 발생 시 롤백
         console.error("Error:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -49,8 +45,8 @@ app.post('/create', async (req, res) => {
 // 데이터 목록 조회 엔드포인트
 app.get('/list', async (req, res) => {
     try {
-        // 비동기 처리를 위해 await 사용
-        const result = await db.executeFetch("SELECT * FROM board");
+        // 비동기 처리를 위해 await 사용하지 않음
+        const result = db.executeFetch("SELECT * FROM board");
         // 결과를 필요한 형태로 매핑
         res.json(result.map(row => ({ id: row.id, title: row.title, message: row.message })));
     } catch (error) {
@@ -62,17 +58,12 @@ app.get('/list', async (req, res) => {
 // 데이터 삭제 엔드포인트
 app.post('/delete', async (req, res) => {
     try {
-        
         const id = req.body.id;
         // SQL 쿼리에 prepared statement 사용
         const sql = `DELETE FROM board WHERE id=?`;
-
-        await db.beginTransaction(); // 트랜잭션 시작
-        await db.execute(sql, [id]);
-        await db.commit(); // 트랜잭션 커밋
+        db.execute(sql, [id]);
         res.json({ 'result': 'success' });
     } catch (error) {
-        await db.rollback(); // 오류 발생 시 롤백
         console.error("Error:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
@@ -86,12 +77,9 @@ app.post('/modify', async (req, res) => {
         const id = req.body.id;
         // SQL 쿼리에 prepared statement 사용
         const sql = `UPDATE board SET title=?, message=? WHERE id=?`;
-        await db.beginTransaction(); // 트랜잭션 시작
-        await db.execute(sql, [title, message, id]);
-        await db.commit(); // 트랜잭션 커밋
+        db.execute(sql, [title, message, id]);
         res.json({ 'result': 'success' });
     } catch (error) {
-        await db.rollback(); // 오류 발생 시 롤백
         console.error("Error:", error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
