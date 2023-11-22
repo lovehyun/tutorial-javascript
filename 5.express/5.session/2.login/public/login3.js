74b5e87721ea69418e568f14d124d0a93f5d597d
Su-Hyun Park <lovehyun@hotmail.com>
2023-11-23 01:25:02 +0900

    [DEV] 마이너 설명 추가

--- 5.express/5.session/2.login/public/login3.js
+++ 5.express/5.session/2.login/public/login3.js
@@ -35,14 +35,46 @@ function login() {
         },
         body: JSON.stringify({ username, password }),
     })
-    .then(response => response.json())
-    .then(data => {
+    .then(response => response.json()) // 서버로 부터 받은 메세지를 json 으로 파싱
+    .then(data => { // 파싱 한 데이터를 다시 프로세싱
         if (data.message === '로그인 성공!') {
             // 로그인 성공한 경우에만 checkLoginStatus 호출
             checkLoginStatus();
         } else {
             alert('로그인 실패');
         }
+    });
+}
+
+function login2() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+    })
+    .then(response => { // 응답값을 보고 처리도 가능 함
+        if (response.status === 200) {
+            // 로그인 성공
+            return response.json();
+        } else {
+            // 로그인 실패 또는 다른 상태 코드 처리
+            throw new Error('로그인 실패');
+        }
+    })
+    .then(data => { // 파싱 한 데이터를 다시 프로세싱
+        // 로그인 성공한 경우에만 checkLoginStatus 호출
+        console.log(data.message);
+        checkLoginStatus();
+    })
+    .catch(error => {
+        // 여기에서 로그인 실패 또는 다른 오류 처리
+        console.error('로그인 오류:', error);
+        alert('로그인 실패');
     });
 }
 
@@ -58,6 +90,7 @@ function logout() {
     });
 }
 
+// 추가미션
 function performUserActivity() {
     // 사용자의 활동이 있을 때마다 서버에 요청을 보냄
     fetch('/user-activity')
