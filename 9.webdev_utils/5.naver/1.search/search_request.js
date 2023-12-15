8ebf594a4ee275f4d72208435a680c8904924db2
Su-Hyun Park <lovehyun@hotmail.com>
2023-12-15 15:50:12 +0900

    [DEV] naver api request 추가

---
+++ 9.webdev_utils/5.naver/1.search/search_request.js
@@ -0,0 +1,27 @@
+
+const request = require('request');
+require('dotenv').config();
+
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+
+const text = '반갑습니다';
+const encText = encodeURIComponent(text);
+const url = `https://openapi.naver.com/v1/search/blog?query=${encText}`;
+
+const headers = {
+    'X-Naver-Client-Id': client_id,
+    'X-Naver-Client-Secret': client_secret,
+};
+
+request.get({
+    url: url,
+    headers: headers,
+}, (error, response, body) => {
+    if (error) {
+        console.error('Error:', response.statusCode, error);
+    } else {
+        const data = JSON.parse(body);
+        console.log(data);
+    }
+});
