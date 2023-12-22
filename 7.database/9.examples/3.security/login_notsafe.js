35168ffcc4a8fb1fd44f7c3446c53abd5b73a87a
Su-Hyun Park <lovehyun@hotmail.com>
2023-12-23 00:13:42 +0900

    [NEW] DB 보안  - sql injection 예제 추가

---
+++ 7.database/9.examples/3.security/login_notsafe.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const db = new sqlite3.Database(':memory:'); // 메모리에 데이터베이스 생성
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, password TEXT)');
+
+    // 기본 사용자 추가
+    const insert = db.prepare('INSERT INTO users (username, password) VALUES (?, ?)');
+    insert.run('user1', 'password1');
+    insert.run('user2', 'password2');
+    insert.run('user3', 'password3');
+    insert.finalize();
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/signup', (req, res) => {
+    const { username, password } = req.body;
+    db.run('INSERT INTO users (username, password) VALUES (?, ?)', [username, password], (err) => {
+        if (err) {
+            return res.status(500).json({ message: '회원가입 실패' });
+        }
+        res.status(201).json({ message: '회원가입 성공' });
+    });
+});
+
+// Prepared Statement를 사용하지 않아 SQL injection 에 취약한 코드 (' OR 1=1; )
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    const sql = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'";
+    console.log(sql);
+
+    db.get(sql, (err, row) => {
+        if (err) {
+            return res.status(500).json({ message: '로그인 실패' });
+        }
+        if (row) {
+            res.status(200).json({ message: '로그인 성공' });
+        } else {
+            res.status(401).json({ message: '로그인 실패' });
+        }
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`서버가 포트 ${PORT}에서 실행 중입니다.`);
+});
