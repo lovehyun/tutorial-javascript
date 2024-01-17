16c55a9531e0192d425e6376353d1b81e0729ff5
Su-Hyun Park <lovehyun@hotmail.com>
2024-01-17 19:45:39 +0900

    [DEV] api 일관성 있게 프로젝트와 동일한 것으로 변경

--- 9.webdev_utils/7.hiddenapi/12.naver_currency.js
+++ 9.webdev_utils/7.hiddenapi/12.naver_currency.js
@@ -4,10 +4,11 @@ const axios = require('axios');
 // https://api.exchangeratesapi.io/latest?base=USD
 
 // 네이버 API 활용
-const base_url = 'https://m.search.naver.com/p/csearch/content/qapirender.nhn?pkid=141&key=exchangeApiBasic&where=nexearch&q=%ED%95%98%EB%82%98%EC%9D%80%ED%96%89+%ED%99%98%EC%9C%A8%EC%A1%B0%ED%9A%8C&u6=standardUnit&u7=0';
-const usd_url = base_url + '&u3=USD&u4=KRW&u2=1&u1=keb&u8=down&u5=info';
-const jpy_url = base_url + '&u3=JPY&u4=KRW&u2=100&u1=keb&u8=down&u5=info';
+const base_url = 'https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=환율&where=m';
+const usd_url = base_url + '&u1=keb&u6=standardUnit&u7=0&u3=USD&u4=KRW&u8=down&u2=1';
+const jpy_url = base_url + '&u1=keb&u6=standardUnit&u7=0&u3=JPY&u4=KRW&u8=down&u2=100'
 
 axios.get(jpy_url).then(response => {
-    console.log(response.data.itemList.to.price);
+    console.log(response.data);
+    console.log(response.data.country[1].value);
 });
