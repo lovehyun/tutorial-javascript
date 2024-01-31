c97e51e22b977ee6fce0c04365384fd7259da39c
Su-Hyun Park <lovehyun@hotmail.com>
2024-01-31 17:08:11 +0900

    [NEW] index 데모코드 추가

---
+++ 7.database/6.index/datagen.js
@@ -0,0 +1,62 @@
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('mydatabase.db');
+
+const numData = 20_000;
+
+// 테이블 생성
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS employees (
+            id INTEGER PRIMARY KEY,
+            name TEXT,
+            department TEXT,
+            salary INTEGER
+        )
+    `);
+});
+
+// 한글 이름을 랜덤으로 생성하는 함수
+function getRandomName() {
+    const lastNames = ['김', '이', '박', '최', '정']; // 성씨
+    const firstName1 = ['가', '나', '다', '라', '마']; // 이름1
+    const firstName2 = ['바', '사', '아', '자', '차']; // 이름2
+
+    const randomFirstName1 = firstName1[Math.floor(Math.random() * firstName1.length)];
+    const randomFirstName2 = firstName2[Math.floor(Math.random() * firstName2.length)];
+    const randomLastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+
+    return {
+        name: randomLastName + randomFirstName1 + randomFirstName2
+    };
+}
+
+// 랜덤 급여를 생성하는 함수
+function getRandomSalary() {
+    return Math.floor(Math.random() * 90) * 1000 + 10000; // 10,000에서 100,000 사이의 랜덤 급여
+}
+
+// 랜덤 부서를 생성하는 함수
+function getRandomDepartment() {
+    const departments = ['IT', 'HR', 'Engineering', 'Marketing'];
+    return departments[Math.floor(Math.random() * departments.length)];
+}
+
+// numData명의 레코드를 추가
+const insertStmt = db.prepare('INSERT INTO employees (name, department, salary) VALUES (?, ?, ?)');
+for (let i = 0; i < numData; i++) {
+    const { name } = getRandomName();
+    const salary = getRandomSalary();
+    const department = getRandomDepartment();
+    console.log(name, department, salary);
+
+    // insertStmt.run(name, department, salary);
+    insertStmt.run(name, department, salary, function(err) {
+        if (err) {
+            console.error(err.message);
+        }
+    });
+}
+
+// 데이터베이스 연결 종료
+insertStmt.finalize();
+db.close();
---
+++ 7.database/6.index/main.js
@@ -0,0 +1,35 @@
+// main.js
+
+// sqlite3 mydatabase.db
+// -- 타이머 활성화
+// .timer on
+// -- 쿼리 실행
+// SELECT * FROM employees WHERE name = '정다사';
+// -- 타이머 비활성화
+// .timer off
+
+// CREATE INDEX idx_name ON employees(name);
+// CREATE INDEX idx_salary ON employees(salary);
+// DROP INDEX idx_name;
+// DROP INDEX idx_salary;
+
+// SELECT *
+// FROM employees
+// WHERE department = 'HR'
+//   AND salary > (SELECT AVG(salary) FROM employees WHERE department = 'HR');
+
+
+const { connectToDatabase, queryName, queryAll } = require('./queryTime');
+
+const db = connectToDatabase();
+const searchName = '정다사'; // 찾고자 하는 이름을 입력하세요
+
+// 검색 조건 설정
+const searchOptions = {
+    name: '정다사',
+    department: 'HR',
+    salary: 60000
+};
+
+// queryName(db, searchName);
+queryAll(db, searchOptions);
