8f50050eecb676119a4cad027ee2199ddf649c3f
Su-Hyun Park <lovehyun@hotmail.com>
2024-02-05 14:38:14 +0900

    [DEV] 정밀도/정확도 개선  - 무한 스크롤 시 데이터가 있는 갯수만큼 까지만 요청

--- 9.webdev_utils/8.scroll_loading/public/scroll.js
+++ 9.webdev_utils/8.scroll_loading/public/scroll.js
@@ -1,9 +1,12 @@
 let start = 0;
 const itemsPerLoad = 20;
+let end = start + itemsPerLoad;
+
 const container = document.getElementById('scroll-container');
 
 // 초기 데이터 로딩
-fetch(`/get-items?start=${start}&end=${start + itemsPerLoad}`)
+console.log(`초기 데이터 로딩 ${start}..${end}`)
+fetch(`/get-items?start=${start}&end=${end}`)
     .then((response) => response.json())
     .then((items) => {
         items.forEach((item) => {
@@ -20,9 +23,10 @@ fetch(`/get-items?start=${start}&end=${start + itemsPerLoad}`)
 // 무한스크롤 이벤트 감지
 window.addEventListener('scroll', () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-        console.log('화면 끝 스크롤 요청중...');
+        const end = start + itemsPerLoad;
+        console.log(`화면 끝 스크롤 데이터 요청중 ${start}..${end}`);
         // 스크롤이 바닥에 도달하면 데이터 요청
-        fetch(`/get-items?start=${start}&end=${start + itemsPerLoad}`)
+        fetch(`/get-items?start=${start}&end=${end}`)
             .then((response) => response.json())
             .then((items) => {
                 // 받아온 데이터를 화면에 추가
