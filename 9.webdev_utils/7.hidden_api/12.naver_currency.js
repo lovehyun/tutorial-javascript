eb297eac11b9c9e7c3bebfedf0acd59c134c8222
Su-Hyun Park <lovehyun@hotmail.com>
2024-02-19 00:04:37 +0900

    [DEV] axios 외에 node-fetch 추가

--- 9.webdev_utils/7.hidden_api/12.naver_currency.js
+++ 9.webdev_utils/7.hidden_api/12.naver_currency.js
@@ -12,3 +12,20 @@ axios.get(jpy_url).then(response => {
     console.log(response.data);
     console.log(response.data.country[1].value);
 });
+
+
+// const fetch = require('node-fetch');
+
+// // 네이버 API 활용
+// const base_url = 'https://m.search.naver.com/p/csearch/content/qapirender.nhn?key=calculator&pkid=141&q=환율&where=m';
+// const usd_url = base_url + '&u1=keb&u6=standardUnit&u7=0&u3=USD&u4=KRW&u8=down&u2=1';
+// const jpy_url = base_url + '&u1=keb&u6=standardUnit&u7=0&u3=JPY&u4=KRW&u8=down&u2=100';
+
+// fetch(jpy_url)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+//         console.log(data.country[1].value);
+//     })
+//     .catch(error => console.error('Error:', error));
+
